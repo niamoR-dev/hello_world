d5076cb6d73502f719ba4a9bcdb3b7beea1415b2
DESKTOP-5L7N2A8\ib <lemaire.romain37@gmail.com>
2021-04-09 16:10:16 +0200

    first commit

---
+++ src/fr/ibformation/helloworld/Laucher.java
@@ -0,0 +1,16 @@
+package fr.ibformation.helloworld;
+
+import java.util.Scanner;
+
+public class Laucher {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World Bitches and Renaud !");
+
+	System.out.println("Finally Hello Renaud the bitch and the others !!!");
+		Scanner scanner;
+		
+		
+	}
+
+}
